73739efd21e894fa99dbc4574a56e8dbe231ae91
Mohammad Majidi <mohammad_majidi@brown.edu>
2023-04-10 09:34:51 -0400

    erpe.m just added

---
+++ src/erpe.m
@@ -0,0 +1,18 @@
+
+
+clear all; 
+clc;
+
+[t_inf,tb,p_inf,p_v,mul,rol,cc,lambdal,lw,alpha,rdot,ri,ro]=parameters();
+% k=1e-6; %time-step
+% N=10;
+% n=N+2; % number of grid points 
+% point = 1:1:n; 
+% z = cos(0.5*pi - 0.5*pi*((point-1)/(n-1))); % collocation points 
+% %Initial temeprature field and its first and second derivatives
+% [T,Tz,Tzz] = initial(n,z,t_inf);
+
+[h,t_r,t,r]=rk4(ri,rdot,mul,rol,p_v,p_inf,ro,cc);
+
+plot(t/0.001,r(1,:)/ro,LineWidth=1.5,Color='black')
+
